28dfcecfef5bc259331812e7d700125941af1d9b
wbozonshop <126032198+wbozonshop@users.noreply.github.com>
2023-04-01 10:40:49 +0300

    Update itsmysklad.js

--- itsmysklad.js
+++ itsmysklad.js
@@ -1,3 +1,3 @@
-const banner = document.createElement('div');
+const banner = document.createElement('div class="custom-container p-0"');
 banner.innerHTML = 'banner';
 document.body.insertBefore(banner, document.body.firstChild);
