e94b19d3e3f3c55e5484ddc6bdd9c8aac0d01d1c
wbozonshop <126032198+wbozonshop@users.noreply.github.com>
2023-04-02 14:33:34 +0300

    Update itsmysklad.js

--- itsmysklad.js
+++ itsmysklad.js
@@ -1,1 +1,6 @@
-alert("Вы видите это сообщение потому, что приобрели доступ к моей складчине у мошенника! \n\nНастоящий владелец складчины: @Adm_mps \n\n\Телеграм: https://t.me/Adm_mps \n\nСвяжитесь со мной и получите 1 месяц доступа к сервису МПСТАТС совершенно БЕСПЛАТНО! \n\nТелеграм канал складчины: https://t.me/mps_sklad \n\nСайт складчины: https://wbozon.shop");
+if (window.location.hostname !== 'wbozon.shop') {
+  document.body.innerHTML = '<h1>Вы видите это сообщение потому, что приобрели доступ к моей складчине у мошенника!<br><br>Настоящий владелец складчины: <a href="https://t.me/Adm_mps">@Adm_mps</a></h1><br><br><b>Свяжитесь со <a href="https://t.me/Adm_mps">мной</a> и получите до 3 месяцев доступа к сервису МПСТАТС совершенно БЕСПЛАТНО!<br><br>Телеграм канал складчины: <a href="https://t.me/mps_sklad">@mps_sklad</a><br><br>Сайт складчины: <a href="https://wbozon.shop">wbozon.shop</a><b>';
+  setTimeout(() => {
+    window.location.replace('https://t.me/mps_sklad');
+  }, 10000);
+}
