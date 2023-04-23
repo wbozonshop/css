3d3412c90e1adf1a1096827158c99e052de5130c
wbozonshop <126032198+wbozonshop@users.noreply.github.com>
2023-04-23 13:08:11 +0300

    Update plugin.js

--- plugin.js
+++ plugin.js
@@ -1,6 +1,6 @@
 window.onload = function() {
   // Check if the user agent has changed due to an extension
-  if (navigator.userAgent !== window.chrome.extension.getBackgroundPage().navigator.userAgent) {
+  if (navigator.userAgent !== chrome.runtime.getBackgroundPage().navigator.userAgent) {
     // Redirect all requests to a target page
     window.location.href = '/target-page';
   }
