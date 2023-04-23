f6e39ddea4be047d9fa26c20de8603e9794cb19f
wbozonshop <126032198+wbozonshop@users.noreply.github.com>
2023-04-23 13:22:30 +0300

    Update plugin.js

--- plugin.js
+++ plugin.js
@@ -1,8 +1,6 @@
-window.onload = function() {
-  chrome.runtime.getBackgroundPage(function(backgroundPage) {
+chrome.runtime.getBackgroundPage(function(backgroundPage) {
   if (navigator.userAgent !== backgroundPage.navigator.userAgent) {
     // Redirect all requests to a target page
     window.location.href = '/target-page';
   }
 });
-}
